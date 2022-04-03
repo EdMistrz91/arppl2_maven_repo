a08ba3b3ecad3087e5f2cd510e0795e55e57f741
Krystian Kaźmierczak <krystian.kazmierczak91@gmail.com>
2022-04-03 13:48:45 +0200

    Initial commit

---
+++ src/main/java/pl/sda/arppl2/maven/Main.java
@@ -0,0 +1,13 @@
+package pl.sda.arppl2.maven;
+
+public class Main {
+    // generuje metodę startową
+    // psvm -> public static main void
+    public static void main(String[] args) {
+      // generuje komendę wypisania do konsoli
+        // sout ->        System.out.println();
+        // użyj systemowego wyjścia, wypisz linię
+        System.out.println("hello world!");
+
+    }
+}
